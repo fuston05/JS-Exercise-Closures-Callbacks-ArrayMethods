7a1cef27a4a9443d7c5db837276e1bf3b67615a6
fuston05 <fuston05@yahoo.com>
2019-11-01 16:46:27 -0400

    passed 'getRunnersByShirtSize'

--- index.js
+++ index.js
@@ -246,8 +246,11 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  const shirtArray= runners.filter(function(ele){
+    return tShirtSize === ele.shirt_size;
+  } );
+  return shirtArray;
 }
 
 /**
