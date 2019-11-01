4cd6ea03daeda7b9adeb1616e3e7e50d3366009a
fuston05 <fuston05@yahoo.com>
2019-11-01 14:05:56 -0400

    passed 'firstNamesAllCaps'

--- index.js
+++ index.js
@@ -225,8 +225,12 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const fNamesArray= runners.map( (ele) => {
+    let upperEle = ele.first_name.toUpperCase();
+    return upperEle;
+  } );
+  return fNamesArray;
 }
 
 /**
